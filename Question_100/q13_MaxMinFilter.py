db1d3ed2cd6e56408cda5fcbdf3327c74f1684e3
Zpadger <31385823+Zpadger@users.noreply.github.com>
2019-11-28 17:10:35 +0800

    correct anwser

--- Question_100/q13_MaxMinFilter.py
+++ Question_100/q13_MaxMinFilter.py
@@ -4,30 +4,72 @@
 import cv2
 import numpy as np
 
-img=cv2.imread("imori.jpg")
-h,w,c=img.shape
+# img=cv2.imread("imori.jpg")
+# h,w,c=img.shape
 
-#filter
-K_size=3
+# #filter
+# K_size=3
 
-K=np.diag([1]*K_size).astype(np.float)
-K/=K_size
+# K=np.diag([1]*K_size).astype(np.float)
+# K/=K_size
 
-#zero padding
-pad=K_size//2
-out=np.zeros((h+pad*2,w+pad*2,c),dtype=np.float)
-out[pad:pad+h,pad:pad+w]=img.copy().astype(np.float)
+# #zero padding
+# pad=K_size//2
+# out=np.zeros((h+pad*2,w+pad*2,c),dtype=np.float)
+# out[pad:pad+h,pad:pad+w]=img.copy().astype(np.float)
 
-tmp=out.copy()
+# tmp=out.copy()
 
-for y in range(h):
-    for x in range(w):
-        for c in range(c):
-            out[pad+y,pad+x,c]=np.max(tmp[y:y+K_size,x:x+K_size,c])\
-                               -np.min(tmp[y:y+K_size,x:x+K_size,c])
+# for y in range(h):
+#     for x in range(w):
+#         for c in range(c):
+#             out[pad+y,pad+x,c]=np.max(tmp[y:y+K_size,x:x+K_size,c])\
+#                                -np.min(tmp[y:y+K_size,x:x+K_size,c])
 
 
-out = out[pad:pad+h, pad:pad+w].astype(np.uint8)
+# out = out[pad:pad+h, pad:pad+w].astype(np.uint8)
+
+# Gray scale
+def BGR2GRAY(img):
+    b = img[:, :, 0].copy()
+    g = img[:, :, 1].copy()
+    r = img[:, :, 2].copy()
+
+    # Gray scale
+    out = 0.2126 * r + 0.7152 * g + 0.0722 * b
+    out = out.astype(np.uint8)
+
+    return out
+
+# max-min filter
+def max_min_filter(img, K_size=3):
+    #H, W, C = img.shape
+    H, W =img.shape
+
+    # Zero padding
+    pad = K_size // 2
+    out = np.zeros((H + pad * 2, W + pad * 2), dtype=np.float)
+    out[pad: pad + H, pad: pad + W] = gray.copy().astype(np.float)
+    tmp = out.copy()
+
+    # filtering
+    for y in range(H):
+        for x in range(W):
+            out[pad + y, pad + x] = np.max(tmp[y: y + K_size, x: x + K_size]) - np.min(tmp[y: y + K_size, x: x + K_size])
+
+    out = out[pad: pad + H, pad: pad + W].astype(np.uint8)
+
+    return out
+
+
+# Read image
+img = cv2.imread("imori.jpg").astype(np.float)
+
+# grayscale
+gray = BGR2GRAY(img)
+
+# Max-Min filtering
+out = max_min_filter(gray, K_size=3)
 
 #save result
 cv2.imwrite("out.jpg", out)
